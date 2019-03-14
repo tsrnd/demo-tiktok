4708a71623bede5f42b908f9114c909994f1dc2d
Kiet Vo A <kiet.vo@asiantech.vn>
2019-03-14 17:22:19 +0700

    First init app

---
+++ app/src/main/java/com/cellhubs/democameraapp/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.cellhubs.democameraapp
+
+import android.app.Application
+import timber.log.Timber
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
